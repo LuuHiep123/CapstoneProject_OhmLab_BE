f05ca614157aa12d6c21ab623e7f14094cdab90f
HSL-2003 <hoangsonlam97@gmail.com>
2025-10-01 23:14:23 +0700

    FIx BUg

--- OhmLab_FUHCM_BE/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ OhmLab_FUHCM_BE/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2565d6726cbc6168b4b6a617e5efd761dd1bba64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0c428a522480cee57a945c4f442dfc91be5afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
